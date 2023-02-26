c3551a93a2aa92b054f16af27dd0a1f2dfe9d5da
Adrian Medina <adrian@wikipunk.net>
2023-02-26 17:30:44 -0500

    use FastF1 via libpython-clj
    requires python modules: fastf1, numpy, and pandas

--- dev/dev.clj
+++ dev/dev.clj
@@ -19,15 +19,18 @@
    [com.stuartsierra.component :as com]
    [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
    [com.walmartlabs.schematic :as sc]
+   [libpython-clj2.require :refer [require-python]]
+   [libpython-clj2.python :as py]
    [net.wikipunk.boot]
    [net.wikipunk.chat :as chat]   
    [net.wikipunk.mop :as mop :refer [isa? descendants parents ancestors]]
    [net.wikipunk.openai :as openai]
    [net.wikipunk.rdf :as rdf :refer [doc]]
-   [net.wikipunk.temple :as temple]
-   [zprint.core :as zprint]
+   [net.wikipunk.temple :as temple]   
    [net.wikipunk.f1.boot :as boot]
+   [net.wikipunk.f1.fastf1 :as fastf1]
    [net.wikipunk.rdf.f1]
+   [net.wikipunk.rdf.ergast]
    [net.wikipunk.rdf.auto-av]
    [net.wikipunk.rdf.auto-cc]
    [net.wikipunk.rdf.auto-ev]
@@ -35,8 +38,11 @@
    [net.wikipunk.rdf.auto-ti]
    [net.wikipunk.rdf.auto-vc]
    [net.wikipunk.rdf.auto-vp]
-   [net.wikipunk.rdf.auto-vs])
+   [net.wikipunk.rdf.auto-vs]
+   [zprint.core :as zprint])
   (:refer-clojure :exclude [isa? descendants parents ancestors]))
+
+(require-python '[fastf1 :as ff1])
 
 (set-init
   (fn [_]
@@ -52,3 +58,12 @@
   [& body]
   `(do (@user/reveal (do ~@body))
        true))
+
+(comment
+  ;; fastf1.get_session_testing(2023, 1, 1)
+  (get-in system [:fastf1 :testing [2023 1] 1])
+
+  (let [df (py/get-attr @(get-in system [:fastf1 :testing [2023 1] 1]) "laps")]
+    (into []
+          (map #(zipmap (seq df) %))
+          (numpy/array df))))
---
+++ src/net/wikipunk/f1/fastf1.clj
@@ -0,0 +1,47 @@
+(ns net.wikipunk.f1.fastf1
+  "https://github.com/theOehrly/Fast-F1"
+  (:require
+   [clojure.java.io :as io]
+   [com.stuartsierra.component :as com]
+   [libpython-clj2.require :refer [require-python]]
+   [libpython-clj2.python :as py]))
+
+(require-python '[fastf1 :as ff1])
+(require-python '[fastf1.Cache :as cache])
+(require-python '[fastf1.core.Session :as session])
+(require-python 'pandas)
+(require-python 'numpy)
+
+(defrecord FastF1 [cache-dir events testing]
+  com/Lifecycle
+  (start [this]
+    (cache/enable_cache (.getPath (doto (io/file cache-dir) (.mkdirs))))
+    (let [events (reduce (fn [events [year gp]]
+                           (assoc events
+                                  [year gp]
+                                  (reduce (fn [sessions session-number]
+                                            (assoc sessions session-number
+                                                   (delay
+                                                     (doto (ff1/get_session year gp session-number)
+                                                       (session/load)))))
+                                          {}
+                                          (range 1 6))))
+                         {} events)
+          testing (reduce (fn [events [year test-number]]
+                            (assoc events
+                                   [year test-number]
+                                   (reduce (fn [sessions session-number]
+                                             (assoc sessions session-number
+                                                    (delay
+                                                      (doto (ff1/get_testing_session year test-number session-number)
+                                                        (session/load)))))
+                                           {}
+                                           (range 1 4))))
+                          {} testing)]
+      (assoc this
+             :events events
+             :testing testing)))
+  (stop [this]
+    this))
+
+
