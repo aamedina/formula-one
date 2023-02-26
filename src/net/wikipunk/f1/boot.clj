88bb70e891b7bf27f4107c3c0617fe24808d216b
Adrian Medina <adrian@wikipunk.net>
2023-02-26 12:59:08 -0500

    depend on io.github.aamedina/auto

--- src/net/wikipunk/f1/boot.clj
+++ src/net/wikipunk/f1/boot.clj
@@ -1,5 +1,15 @@
 (ns net.wikipunk.f1.boot
-  {:rdf/type :jsonld/Context})
+  {:rdf/type :jsonld/Context}
+  (:require
+   [net.wikipunk.auto.boot]
+   [net.wikipunk.rdf.auto-av]
+   [net.wikipunk.rdf.auto-cc]
+   [net.wikipunk.rdf.auto-ev]
+   [net.wikipunk.rdf.auto-mo]
+   [net.wikipunk.rdf.auto-ti]
+   [net.wikipunk.rdf.auto-vc]
+   [net.wikipunk.rdf.auto-vp]
+   [net.wikipunk.rdf.auto-vs]))
 
 (def f1
   {:rdf/type    :rdfa/PrefixMapping
