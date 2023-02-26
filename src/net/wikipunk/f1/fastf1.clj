b5d4107a0a5adea99f4939ab5afcb359e2c51d1c
Adrian Medina <adrian@wikipunk.net>
2023-02-26 17:49:59 -0500

    require np-array

--- src/net/wikipunk/f1/fastf1.clj
+++ src/net/wikipunk/f1/fastf1.clj
@@ -4,7 +4,8 @@
    [clojure.java.io :as io]
    [com.stuartsierra.component :as com]
    [libpython-clj2.require :refer [require-python]]
-   [libpython-clj2.python :as py]))
+   [libpython-clj2.python :as py]
+   [libpython-clj2.python.np-array]))
 
 (require-python '[fastf1 :as ff1])
 (require-python '[fastf1.Cache :as cache])
